916c5731f69f571a8b9a1fe94c1644e4bb3a87d5
sakshiiii12 <sakshiiii761@gmail.com>
2025-03-30 13:04:11 +0530

    AreaOfRectangle

---
+++ Basic Input_Output/AreaRectangle.py
@@ -0,0 +1,10 @@
+#Python program to calculate the area of a rectangle.
+
+length = int(input("Enter the length of the rectangle:"))
+width = int(input("Enter the width of the rectangle:"))
+
+#Area of a rectangle is length * width
+
+area = (length*width)
+
+print("Area of a Rectangle:",area)
