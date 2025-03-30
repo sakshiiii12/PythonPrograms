7a0dda98feb823ba963ab3a6a8bf3f114d90b7a7
sakshiiii12 <sakshiiii761@gmail.com>
2025-03-30 12:53:31 +0530

    SumTwoIntegers

---
+++ Basic Input_Output/SumTwoIntegers.py
@@ -0,0 +1,6 @@
+#Python program to take two integers as input and print their sum.
+
+num1 = int(input("Enter the number:"))
+num2 = int(input("Enter the number:"))
+sum = num1 + num2
+print("Sum of two integers is:",sum)
