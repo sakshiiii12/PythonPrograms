9b1b520ccd7b8739c10db18a3323fb6463303323
sakshiiii12 <sakshiiii761@gmail.com>
2025-03-31 18:47:41 +0530

    printprogram

---
+++ Control Structure(if,else and loops)/11.checknum.py
@@ -0,0 +1,10 @@
+#Python program to check whether a number is positive, negative, or zero.
+
+num = int(input("Enter the number to check:"))
+
+if(num>0):
+    print("The given number is positive number")
+elif(num<0):
+    print("The given number is negative number")
+else:
+    print("The given number is zero")
