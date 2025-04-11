95d8393e3e150d6e48dc42da85dff8e63cc098e3
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-11 14:24:16 +0530

    printprogram

---
+++ Dictionaries/63.getKeysDict.py
@@ -0,0 +1,7 @@
+#Python program to get the keys of a dictionary.
+
+dict = {'Name': 'Ram Sharma', 'RollNo.': 19, 'Class': 10, 'Math': 89, 'Science': 92, 'Sst': 70, 'English':95, 'Hindi':97}
+print("Dictionary:",dict)
+for keys in dict.keys():
+    print(keys,end=" ")
+
---
+++ Dictionaries/64.getValuesDict.py
@@ -0,0 +1,6 @@
+#Python program to get the values of a dictionary.
+
+dict = {'Name': 'Ram Sharma', 'RollNo.': 19, 'Class': 10, 'Math': 89, 'Science': 92, 'Sst': 70, 'English':95, 'Hindi':97}
+print("Dictionary:",dict)
+for values in dict.values():
+    print(,end=" ")
