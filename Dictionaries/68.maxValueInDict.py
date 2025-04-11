147df18ec983f4b50d30eb3f4ad4a803dfb22bd6
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-11 14:44:12 +0530

    printprogram

---
+++ Dictionaries/68.maxValueInDict.py
@@ -0,0 +1,5 @@
+#Python program to find the maximum value in a dictionary.
+
+dict1 = {'Peas':50, 'Onion':70, 'Cabbage':100, 'Tomoato':90, 'Gralic':70}
+max_value = max(dict1.values())
+print("Maximun value in the dictionary:",max_value)
