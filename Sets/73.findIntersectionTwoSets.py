2e2db0bd6f6db1cab53a198f7bd712995de836c0
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-12 21:11:40 +0530

    printprogram

---
+++ Sets/73.findIntersectionTwoSets.py
@@ -0,0 +1,6 @@
+#Python program to find the intersection of two sets.
+
+set1 = {1,2,3,4,5}
+set2 = {2,4,1,6,7}
+intersection_set = set1.intersection(set2)
+print("The Intersection Of Two Sets:",intersection_set)
