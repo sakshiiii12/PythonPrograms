c2c5fae9e9588f821174af47711e87360d55fe84
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-12 20:51:24 +0530

    printprogram

---
+++ Sets/71.createSetAndAddElements.py
@@ -0,0 +1,13 @@
+#Python program to create a set and add elements to it.
+
+#First Way To Add Elements In A Set
+set = {1,2,3}
+print(set)
+set.add(4)
+print(set)
+set.add(5)
+print(set)
+
+#Second Way To Add Elements In A Set
+set.update({6,7,8,9,10})
+print(set)
