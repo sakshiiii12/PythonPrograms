6244802b0037268d78ce972e0c58f9c933fe0835
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-13 21:38:56 +0530

    printprogram

---
+++ Sets/77.checkElementExists.py
@@ -0,0 +1,8 @@
+#Python program to check if an element exists in a set.
+
+set = {4,5,6,7,8,9}
+check = int(input("Enter the element:"))
+if(check in set):
+     print("The Elements Exists In A Set")
+else:
+     print("The Elements Not Exists In A Set")
