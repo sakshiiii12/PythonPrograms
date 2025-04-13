89b6aa6c65079c29ffbf9897d4dc3cfa7cb8359a
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-13 21:51:34 +0530

    printprogram

---
+++ Sets/80.findSymmetricDifferenceBetweenTwoSets.py
@@ -0,0 +1,6 @@
+#Python program to find the symmetric difference between two sets.
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+sy_diff = set1.symmetric_difference(set2)
+print("Symmetric difference between two sets are:",sy_diff)
