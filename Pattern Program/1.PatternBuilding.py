3f7602fbdea4355987387f3508e1b3a1405ea6d4
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-27 14:43:01 +0530

    printprogram

---
+++ Pattern Program/1.PatternBuilding.py
@@ -0,0 +1,13 @@
+#Write a python program for this pattern
+'''
+*****
+*****
+*****
+*****
+*****
+'''
+
+for i in range(1,6): #rows
+    for j in range(1,6): #columns
+        print("*",end="")
+    print()
