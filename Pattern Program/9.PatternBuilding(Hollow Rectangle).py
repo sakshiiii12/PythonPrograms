aabc1792f8b44be957a9d5e481b6734ac5a1b80c
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-27 16:10:37 +0530

    printprogram

---
+++ Pattern Program/9.PatternBuilding(Hollow Rectangle).py
@@ -0,0 +1,18 @@
+#Write a python program for this pattern
+'''
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+'''
+
+rows = 5
+cols = 5
+for i in range(rows):
+    for j in range(cols):
+        if i == 0 or i == rows-1 or j == 0 or j == cols-1:
+            print('*',end=" ")
+        else:
+            print(' ',end=" ")
+    print()
