38e31ed088dfbff671f4513ac250bd7c81fe554e
sakshiiii12 <sakshiiii761@gmail.com>
2025-04-29 18:09:46 +0530

    printprogram

--- Pattern Program/8.PatternBuilding(Diamond).py
+++ Pattern Program/8.PatternBuilding(Diamond).py
@@ -21,7 +21,7 @@ for i in range(1,5+1):
     print()
 
 #Lower Half
-for i in range(5,0,-1):
+for i in range(4,0,-1):
     for j in range(1,5+1):
         if j < 5 - i + 1:
             print(" ",end="")
